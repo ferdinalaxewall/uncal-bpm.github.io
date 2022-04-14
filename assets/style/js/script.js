4f4d0941e2236b929be596ef9273ccbc46b1a9ae
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2022-04-14 19:55:24 +0700

    Add files via upload

--- assets/style/js/script.js
+++ assets/style/js/script.js
@@ -93,4 +93,83 @@ $(document).ready(function(){
 
     $(".list-sub-item").parent().addClass("has-child");
 
+
+    $('.element-item').sortable({
+        connectWith: '.flow-diagram',
+        receive: function (event, ui) {
+        }
+      });
+      
+    //   $('.flow-diagram').sortable({
+    //     appendTo: document.body,
+    //     items: '.element-item',
+    //     connectWith: '.element-item',
+    //     receive: function (event, ui) {
+    //     //   console.log(event, ui.item);
+    //       //ui.item.remove(); // remove original item
+    //     }
+    //   });
+
+    // $( function() {
+    //     $( ".elements-list, .flow-diagram" ).sortable({
+    //       connectWith: ".flow-diagram"
+    //     }).disableSelection();
+    //   } );
+
+
+    $(".elements-list .element-item").draggable({
+        helper : 'clone',
+        cursor: "move", 
+        cursorAt: { top: 25, left: 90 },
+        snap : ".canvas",
+        drag : function(e, ui){
+            $(".flow-diagram").css({
+                "min-width" : "100%",
+                "height" : "490px"
+            });
+        },
+        stop : function (e, ui) {
+            $(".flow-diagram").css({
+                "width" : "auto"
+            });
+            // $(".canvas").append("<ul class='flow-diagram'></ul><br>")
+        }
+    });
+
+    
+    // if (!$(".flow-diagram").find(".element-item").length) {
+    //     console.log("kosong");
+    // }else{
+    //     console.log("tidak kosong   ")
+    // }
+
+    // $(".flow-diagram").click(function(e){
+    //     e.preventDefault();
+    //     console.log("Ok");
+    // });
+    
+    // $(".flow-diagram").after("<br>")
+
+    // $(".canvas").droppable({
+    //     accept : ".element-item",
+    //     drop : function(ev, ui){
+    //             var droppedItem = $(ui.draggable).clone();
+    //         $(this).append("<ul class='flow-diagram'></ul>");
+    //         console.log(droppedItem);
+    //         // if($(this).children().length > 0){
+    //         // }
+    //     }
+    // });
+    
+    // $(".flow-diagram").sortable();
+
+    $(".flow-diagram").droppable({
+        accept : ".element-item",
+        drop : function (ev, ui) {
+            var droppedItem = $(ui.draggable).clone();
+            $(this).append(droppedItem);   
+
+        }
+    })
+
 });
